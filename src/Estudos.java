fcc388a9d95cf06d3600bcba2c68fcf232f0e0a8
beatriz-nagae <bh.prestashop@gmail.com>
2025-09-21 19:10:03 -0300

    Primeiro Commit

---
+++ src/Estudos.java
@@ -0,0 +1,14 @@
+import java.util.Scanner;
+
+public class Estudos {
+    public static void main(String[] args) {
+
+        String anotacoes;
+
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Insira as anotações:");
+       anotacoes = scanner.nextLine();
+        System.out.print("anotacoes" + anotacoes);
+
+    }
+}
