130d47df716d44efa45b9ed3b8b042f9fa0d7eba
bia nagae <138304130+beatriz-nagae@users.noreply.github.com>
2025-09-24 21:38:18 -0300

    Update Conversao.java

--- src/Conversao.java
+++ src/Conversao.java
@@ -9,7 +9,7 @@ double fahrenheit;
         Scanner termometro = new Scanner(System.in);
         System.out.println("Insira a temperatura em C: ");
         tempcelsius = termometro.nextDouble();
-        double temperatura_convertida = (tempcelsius * 1.8) + 32.0;
+        double temperatura_convertida = (tempcelsius * 1.8) + 32.0; // conversão celcius e fahrenheight farengheig faren rapaz como que escreve
         System.out.println("A temperatura convertida é: " + temperatura_convertida);
 
 
