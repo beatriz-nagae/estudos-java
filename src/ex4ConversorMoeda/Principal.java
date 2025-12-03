19993f008de1795fbdffe99ad4c2c09f6636c172
bia nagae <138304130+beatriz-nagae@users.noreply.github.com>
2025-12-03 14:55:42 -0300

    Refactor currency converter to accept user input

--- src/ex4ConversorMoeda/Principal.java
+++ src/ex4ConversorMoeda/Principal.java
@@ -1,16 +1,23 @@
 package ex4ConversorMoeda;
 
+import java.util.Scanner;
+
 public class Principal {
     public static void main(String[] args) {
+
+        Scanner scanner = new Scanner(System.in);
         ConversorMoeda conversor = new ConversorMoeda();
-        conversor.setValorEmDolar(10.0);
-        System.out.println(conversor.converterDolarParaReal());
 
-        //NOTA: não usou set porque não alteramos o atributo,
-        //chamamos um metodo publico (converterDolarParaReal)
-        //que internamente usa o atributo e retorna o resultado.
-double valorEmReais = conversor.converterDolarParaReal();
-        System.out.println(valorEmReais);
+        System.out.println("=== CONVERSOR DÓLAR → REAL ===");
+        System.out.print("Digite o valor em dólar: ");
 
+        double valorEmDolar = scanner.nextDouble();
+        conversor.setValorEmDolar(valorEmDolar);
+
+        double valorEmReais = conversor.converterDolarParaReal();
+
+        System.out.println("Valor convertido em reais: R$ " + valorEmReais);
+
+        scanner.close();
     }
 }
