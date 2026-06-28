64de2835ced4e9287cb18a35927bdea79fb62b88
bia nagae <138304130+beatriz-nagae@users.noreply.github.com>
2026-06-27 21:46:34 -0300

    Add NumeroPar class to check if a number is even

---
+++ src/NumeroPar.java
@@ -0,0 +1,11 @@
+public class NumeroPar {
+    public static void main(String[] args) {
+        int numero = 18;
+
+        if (numero % 2 == 0) {
+            System.out.println(numero + " é par.");
+        } else {
+            System.out.println(numero + " é ímpar.");
+        }
+    }
+}
